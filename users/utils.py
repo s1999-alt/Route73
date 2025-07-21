fa8564c2f589d82ffa20b5a165c9dae46c16f91b
Abhijith M R <abhijithmr581@gmail.com>
2025-07-21 14:17:39 +0530

    send otp added

--- users/utils.py
+++ users/utils.py
@@ -4,12 +4,22 @@ import datetime
 from django.utils import timezone
 from rest_framework_simplejwt.tokens import RefreshToken
 from datetime import timedelta
+import requests
+from decouple import config
 
 
 # Placeholder for SMS sending integration (e.g., Twilio)
-def send_sms(phone_number, message):
-    # Integrate with SMS provider here
-    print(f"Sending SMS to {phone_number}: {message}")
+def send_sms(phone_number, otp_code):
+    api_key = config('API_KEY')
+    otp_template_name = config('OTP_TEMPLATE_NAME')
+    url = f"https://2factor.in/API/V1/{api_key}/SMS/{phone_number}/{otp_code}/{otp_template_name}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raises an HTTPError for bad responses (4xx or 5xx)
+        print(f"Successfully sent OTP to {phone_number}")
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to send OTP to {phone_number}: {e}")
+
 
 
 def generate_and_send_otp(phone_number, expiry_minutes=5):
@@ -25,5 +35,5 @@ def generate_and_send_otp(phone_number, expiry_minutes=5):
     )
 
     # Send OTP via SMS
-    send_sms(phone_number, f"Your OTP code is: {otp_code}")
+    send_sms(phone_number, otp_code)
     return otp_obj
